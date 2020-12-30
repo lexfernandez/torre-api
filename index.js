211a9b31117ba26d90ab22d65a633635edecfeb6
lexfernandez <olorenzo@outlook.com>
2020-12-30 00:09:56 -0600

    Add proxy server

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const request = require("request");
+const cors = require("cors");
+const app = express();
+
+const API_URL = process.env.API_URL;
+const PORT = process.env.PORT;
+
+app.use(cors());
+
+app.use("/", function (req, res) {
+  const uri = API_URL + req.url;
+  const query = req.query;
+  req.pipe(request({ qs: query, uri: uri })).pipe(res);
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
